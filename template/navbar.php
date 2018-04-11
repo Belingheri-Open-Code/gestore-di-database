c7dfeb7210791ed9ae12440f2342f60c3e3f7c3e
Belingheri <33315640+Belingheri@users.noreply.github.com>
2018-04-11 11:55:14 +0200

    Create navbar.php

---
+++ template/navbar.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="./query.php">Belingheri DB</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li ><a href="./query.php">Fai la query</a></li>
+      <li><a href="./show.php">Mostra DB</a></li>
+      <li><a href="./index.php?Logout="  class="btn btn-primary " style="color: black;" >Logout</a></li>
+    </ul>
+  </div>
+</nav>

f5fb3809f9e2d2ecaca44fa42696f93b3107f029
Belingheri <33315640+Belingheri@users.noreply.github.com>
2018-04-12 12:52:49 +0200

    aggiunta navbar e riduzione ricaricamento

--- template/navbar.php
+++ template/navbar.php
@@ -1,11 +1,11 @@
 <nav class="navbar navbar-default">
   <div class="container">
     <div class="navbar-header">
-      <a class="navbar-brand" href="./query.php">Belingheri DB</a>
+      <a class="navbar-brand" href="#">Belingheri DB</a>
     </div>
     <ul class="nav navbar-nav">
-      <li ><a href="./query.php">Fai la query</a></li>
-      <li><a href="./mostra.php">Mostra DB</a></li>
+      <li ><a onclick="mostra()" href="#">Fai la query</a></li>
+      <li><a onclick="nascondi()" href="#">Mostra DB</a></li>
       <li><a href="./index.php?Logout="  class="btn btn-primary " style="color: black;" >Logout</a></li>
     </ul>
   </div>

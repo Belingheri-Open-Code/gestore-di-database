2a73742c6cdfe7cceef97fe7bba9997bf93aa0a0
Diego <33544764+DiegoMartignoni@users.noreply.github.com>
2018-04-13 12:46:27 +0200

    controllo se ha eseguito l'accesso

--- config.php
+++ config.php
@@ -1,19 +1,18 @@
-<!doctype html>
-<html lang="it">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <title>Pannello di configurazione</title>
-  </head>
-  <body>
+<?php
+include("./template/header.php"); session_start();
+?>
+<script>
+    $("#configurazione").addCalss("active");
+</script>
+				</head>
+<?php
+include("./template/navbar.php");
+?>
+    <body>
+    if($_SESSION["loggato"]!=true){
+      header("location: ./index.php");
+    }  
+      
     <?php if($_GET['status'] == '1') { ?>
     <div class="container alert alert-success" role="alert" style="margin-top: 20px;">
     	<strong>Nuovo file generato con successo!</strong>
@@ -47,4 +46,4 @@
         </div>
     </div>
   </body>
-</html>
+</html>

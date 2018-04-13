75f723cdbdbde65b5ccbcc4f0482586334ef24bd
Belingheri <33315640+Belingheri@users.noreply.github.com>
2018-04-13 12:36:04 +0200

    Update credenziali.php

--- credenziali.php
+++ credenziali.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 //credenziali per il form di login	 
-$userFrom="your_username_for_the_Form";
-$passFrom="your_password_for_the_Form";
+$userFrom="default";
+$passFrom="default";
 // credenziali per il DataBase	 
 $servername = "localhost";	
-$username = "your_username";	
-$password = "your_password";
+$username = "default";	
+$password = "";
 
 if (isset($_POST['dbname'])) 
 {

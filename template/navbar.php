4c43bee89c87d630218512b06970ba3440674efe
Belingheri <33315640+Belingheri@users.noreply.github.com>
2018-04-16 17:35:51 +0200

    solved config.php problems

--- template/navbar.php
+++ template/navbar.php
@@ -1,11 +1,21 @@
+<?php
+session_start();
+if($_SESSION["loggato"]!=true){
+	header("location: ./index.php");
+}
+?>
 <nav class="navbar navbar-default">
   <div class="container">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Belingheri DB</a>
+      <a class="navbar-brand" href="./query.php">Belingheri DB</a>
     </div>
     <ul class="nav navbar-nav">
-      <li id="mostrali"><a id="mostra" href="#">Fai la query</a></li>
-      <li id="nascondili"><a id="nascondi" href="#">Mostra DB</a></li>
+    <?php  
+    $output = explode("/",$_SERVER['PHP_SELF']);
+	$page= $output[count($output)-1];
+    ?>
+      <li id="mostrali"><a id="mostra" href='<?php if ($page=='config.php') echo "./query.php"; else echo "#"?> '>Fai la query</a></li>
+      <li id="nascondili"><a id="nascondi" href='<?php if ($page=='config.php') echo "./query.php"; else echo "#"?> '>Mostra DB</a></li>
       <li><a href="./index.php?Logout="  class="btn btn-primary " style="color: black;" >Logout</a></li>
       <li id="configurazione"><a href="./config.php">Modifica file di configurazione</a></li>
     </ul>

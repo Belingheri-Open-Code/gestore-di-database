9973f73ccdc6af127aca4c32f4cab4ad2375aab7
Belingheri <33315640+Belingheri@users.noreply.github.com>
2018-05-24 09:51:52 +0200

    add link to repository

--- template/navbar.php
+++ template/navbar.php
@@ -20,6 +20,9 @@ if ($page!="index.php"){
       <li id="nascondili"><a id="nascondi" href='<?php if ($page=='config.php') echo "./query.php"; else echo "#"?> '>Mostra DB</a></li>
       <li><a href="./index.php?Logout="  class="btn btn-primary " style="color: black;" >Logout</a></li>
       <li id="configurazione"><a href="./config.php">Modifica file di configurazione</a></li>
+	<li>
+	<a href="https://github.com/Belingheri-Open-Code/gestore-di-database" style="text-decoration: none;"><img src="https://camo.githubusercontent.com/7710b43d0476b6f6d4b4b2865e35c108f69991f3/68747470733a2f2f7777772e69636f6e66696e6465722e636f6d2f646174612f69636f6e732f6f637469636f6e732f313032342f6d61726b2d6769746875622d3235362e706e67" height="25" width="25" ></a>
+	</li>
     </ul>
   </div>
 </nav>
